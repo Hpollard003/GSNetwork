dc5dd375a5c32ad61daaab483e0b7d1c15388902
hpollard003 <hpollard003@gmail.com>
2021-08-29 03:38:27 +0200

    The Skeleton is forming

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,23 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import HomePage from './NavPages/HomePage'
+import NavBar from './components/NavBar';
+import MarketPage from './NavPages/MarketPage';
+import JobsPage from './NavPages/JobsPage';
+import InfoPage from './NavPages/InfoPage';
+import BrokerRatingPage from './NavPages/BrokerRatingPage';
 
 const App = () => {
   return (
     <div className="App">
       <Router>
+        <h1>Global_Spy_Network</h1>
+        <NavBar/>
         <Switch>
         <Route exact path='/' component={HomePage} />
+        <Route exact path='/Info' component={InfoPage} />
+        <Route exact path='/Market' component={MarketPage} />
+        <Route exact path='/Jobs' component={JobsPage} />
+        <Route exact path='/Brokers' component={BrokerRatingPage} />
         </Switch>
       </Router>
     </div>
